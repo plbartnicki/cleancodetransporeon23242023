a132156c393b095335881c00be8d6f191d153dfa
bartnicki <lukasz.bartnicki.external@easy-software.com>
2023-10-22 14:36:31 +0200

    code update

--- src/main/java/largesetofclasses/beforerefactor/Main.java
+++ src/main/java/largesetofclasses/beforerefactor/Main.java
@@ -1,17 +1,5 @@
 package largesetofclasses.beforerefactor;
 
-/*
-	Abstraction - GUIElementRenderer
-		AbstrType1 - GUIElementinLinuxStyle
-			ConcreteImpl1_OfAbstrType_1 - ButtonLinux
-			ConcreteImpl1_OfAbstrType_2 - CheckedBoxLinux
-
-		AbstrType2 - GUIElementinWindowsStyle
-			ConcreteImpl1_OfAbstrType_1 - ButtonWindows
-			ConcreteImpl1_OfAbstrType_2 - CheckedWindows
-
-		f(n, k) = 1 + n*k
- */
 
 public class Main
 {
